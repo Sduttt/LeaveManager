3b99b4a92d179efbbc0a92e20bcb3e1f8fea6f0a
Sduttt <subhamdutta588@gmail.com>
2023-11-12 17:36:52 +0530

    connection method updated

--- utils/dbconnect.php
+++ utils/dbconnect.php
@@ -9,8 +9,11 @@ $username = "subham";
 $password = "Sdutta@7866";
 $database = "leavemanager";
 
-$conn = mysqli_connect($server, $username, $password, $database);
+$db = mysqli_init();
+mysqli_ssl_set($db, NULL, NULL, "path/to/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL); // Replace "path/to/BaltimoreCyberTrustRoot.crt.pem" with your SSL certificate path
+
+$conn = mysqli_real_connect($db, $server, $username, $password, $database, 3306); // 3306 is the default MySQL port, change if needed
 
 if (!$conn) {
-    die("Connection error". mysqli_connect_error());
-};
+    die("Connection error: " . mysqli_connect_error());
+}
