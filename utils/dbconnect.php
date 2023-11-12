e38212e26c7c5c5264d5874c00e63b62fa08f45e
Sduttt <subhamdutta588@gmail.com>
2023-11-12 17:02:04 +0530

    updated database credentials

--- utils/dbconnect.php
+++ utils/dbconnect.php
@@ -1,7 +1,12 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
+// $server = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "LeaveManager";
+
+$server = "leavemanager";
+$username = "subham";
+$password = "Sdutta@7866";
 $database = "LeaveManager";
 
 $conn = mysqli_connect($server, $username, $password, $database);
