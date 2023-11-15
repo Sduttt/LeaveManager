5e4d089249bac4813b52086ccdc66aba52f5ba28
Sduttt <subhamdutta588@gmail.com>
2023-11-15 12:10:00 +0530

    certificate path changed 3

--- utils/dbconnect.php
+++ utils/dbconnect.php
@@ -10,7 +10,7 @@ $password = "Sdutta@7866";
 $database = "leavemanager";
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, __DIR__ . "/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn, NULL, NULL, "/home/site/wwwroot/utils/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
 
 $connectionSuccessful = mysqli_real_connect($conn, $server, $username, $password, $database, 3306); // 3306 is the default MySQL port, change if needed
 
